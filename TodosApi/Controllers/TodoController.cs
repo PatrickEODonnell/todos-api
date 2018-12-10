4a15b1cc1357dc0ead6df0b80cca1930abda1135
Patrick ODonnell <pej.odonnell@gmail.com>
2018-12-10 00:06:23 -0500

    Initial Commit

---
+++ TodosApi/Controllers/TodoController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodosApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+            if (_context.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new Todo { Id=Guid.NewGuid().ToString(), Description = "Item1", AssignedTo = "Patrick", DueDate = "2018-12-21", Status="Incomplete" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todo>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todo>> Get(string id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return todoItem;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ActionResult<Todo>> Post(Todo todoItem)
+        {
+            todoItem.Id = Guid.NewGuid().ToString();
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Get", new { id = todoItem.Id }, todoItem);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, Todo todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(todoItem).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Todo>> Delete(string id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+            return todoItem;
+        }
+    }
+}
---
+++ TodosApi/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodosApi.Models
+{
+    public class Todo
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string AssignedTo { get; set; }
+        public string DueDate { get; set; }
+        public string Status { get; set; }
+    }
+}
